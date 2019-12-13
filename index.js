494d9fe0833c0745deea377d7c3e10eeedd9a061
Spencer_Vail <vail.spencer@gmail.com>
2019-12-12 20:27:35 -0700

    first one done

--- index.js
+++ index.js
@@ -41,6 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this. age = age;
+    this.stomach = [];
+  }
+  eat(food) {
+    if (this.stomach.length >= 10) {
+      return;
+    }
+    this.stomach.push(food)
+  }
+
+  poop() {
+    this.stomach.length = 0;
+  }
+
+  toString() {
+    return `${this. name}, ${this.age}`
+  }
 
 }
 
